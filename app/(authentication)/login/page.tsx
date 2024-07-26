1f7ba86448ec18a411e261fad481bd51000ea20b
Yash Raj <ryash072007@gmail.com>
2024-07-26 20:58:01 +0400

    shadcn zod reach-hook-form

--- app/(authentication)/login/page.tsx
+++ app/(authentication)/login/page.tsx
@@ -1,8 +1,19 @@
-import { relative } from 'path'
+"use client" // WHen using forms or usr interaction, use client
 import React from 'react'
-import "./LoginPage.css"
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import { useForm, SubmitHandler } from "react-hook-form"
+// import "./LoginPage.css"
+
+
 
 const LoginPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+      const onSubmit: SubmitHandler<any> = (data:any) => console.log(data)
     return (
         <html lang="en">
             <head>
@@ -12,25 +23,25 @@ const LoginPage = () => {
                     Login
                 </title>
             </head>
-            <body className='body'>
-                <div className='MainContainer'>
-                    <div className='BorderContainer'>
+            <body className='font-mono h-screen w-screen'>
+                <div className='w-min text-center justify-center align-center'>
+                    <div className='border flex flex-col p-14'>
                         <div className='HeadingContainer'>
-                            <h1 className='LoginHeading'>LOGIN</h1>
+                            <h1 className='text-8xl'>LOGIN</h1>
                         </div>
-                        <form action="#ADDLATER" method="post" className='Authentication'>
+                        <form onSubmit={handleSubmit(onSubmit)} method="post" className='column-2'>
                             <div className='HeadingsContainer'>
-                                <div className='Fields'>
-                                    <span className='FieldHeading'>Username</span>
-                                    <input type="text" className="UsernameInputField" id="" />
+                                <div className='p-2'>
+                                    <span className='text-2xl'>Username</span>
+                                    <Input type="text" className="" id="" placeholder='Username' {...register("Username")}/>
                                 </div>
-                                <div className='Fields'>
-                                    <span className='FieldHeading'>Password</span>
-                                    <input type="password" className="PasswordInputField" id="" />
+                                <div className='p-2'>
+                                    <span className='text-2xl'>Password</span>
+                                    <Input type="password" className="" id="" placeholder='Password' {...register("Password")}/>
                                 </div>
-                                <div className='FormButtons'>
-                                    <button type="submit" className='FormButton'>Log in</button>
-                                    <button className='FormButton'>Sign Up</button>
+                                <div className='p-2 flex flex-row justify-center'>
+                                    <Button type="submit" className='text-xl p-4 m-2'>Log in</Button>
+                                    <Button className='text-xl p-4 m-2'>Sign Up</Button>
                                 </div>
                             </div>
                         </form>
